12e4ed2e332d2fb053117f485943e4dbe60a75cf
Stephen McBride <stevemcbride3@gmail.com>
2020-12-01 00:17:21 -0500

    Cleaned up ConnectedIndicator component

--- src/components/ConnectedIndicator.js
+++ src/components/ConnectedIndicator.js
@@ -1,19 +1,4 @@
 export default function ConnectedIndicator(props) {
-    
-    const connectText = (connectStatus) => {
-        switch(connectStatus) {
-            case "connecting":
-                return "CONNECTING"
-                break;
-            case "connected":
-                return "CONNECTED"
-                break;
-            case "disconnected":
-                return "DISCONNECTED"
-                break;
-        }
-    }
-    
     return(
         <div id="connected-indicator" className={props.connectStatus}>
             {props.connectStatus.toUpperCase()}
